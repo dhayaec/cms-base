a350e9e2c7ab10b2047f7be63836d8d0a9dff7c5
Dhayanandhan <dhayaec@gmail.com>
2021-02-17 15:25:34 +0530

    init project

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+
+export const Home = (): JSX.Element => (
+  <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href=" /favicon.ico" />
+    </Head >
+
+    <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+      <h1 className="text-blue-400 text-2xl">
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+      <div className="flex flex-col">
+        <p className="text-indigo-600 py-2">This is an example of base cms package</p>
+      </div>
+    </main>
+
+    <footer>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-xs text-gray-400"
+      >
+        &copy; {new Date().getFullYear()} Base CMS boilerplate
+      </a>
+    </footer>
+  </div >
+)
+
+export default Home
---
+++ pages/utils/utils.test.ts
@@ -0,0 +1,15 @@
+import { add, multiply } from './utils'
+
+describe('utils', () => {
+  describe('add()', () => {
+    test('should add two numbers', () => {
+      expect(add(1, 2 )).toEqual(9)
+    })
+  })
+
+  describe('multiply()', () => {
+    test('should multiply two numbers', () => {
+      expect(multiply(1, 2)).toEqual(2)
+    })
+  })
+})
